f381f17fada4baa476a288ca872b9b77665ac639
igor-kuzmanovic <igor.kuzmanovic.996@gmail.com>
2025-06-19 22:34:54 +0200

    create a contracts project with work in progress methods

---
+++ Contracts/FileData.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+[DataContract]
+public class FileData
+{
+    [DataMember]
+    public byte[] Content { get; set; }
+
+    [DataMember]
+    public byte[] InitializationVector { get; set; }
+}
---
+++ Contracts/IWCFService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security;
+using System.Security.Permissions;
+using System.ServiceModel;
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface IWCFService
+    {
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        string[] ShowFolderContent(string path);
+
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        FileData ReadFile(string path);
+
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        bool CreateFolder(string path);
+
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        bool CreateFile(string path, FileData fileData);
+
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        bool Delete(string path);
+
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        bool Rename(string sourcePath, string destinationPath);
+
+        [OperationContract]
+        [FaultContract(typeof(SecurityException))]
+        bool MoveTo(string sourcePath, string destinationPath);
+    }
+}
